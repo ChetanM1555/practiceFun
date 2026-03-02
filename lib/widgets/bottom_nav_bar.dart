99f9676684b424e99ebaee12a887398929f15ce6
ChetanM1555 <chetanmanilal123@gmail.com>
2026-03-02 19:18:54 +0200

    added navbar

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.stacked_bar_chart),
+          label: "Stats",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: "settings",
+        ),
+      ],
+    );
+  }
+}
